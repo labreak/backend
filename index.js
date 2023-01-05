f8f1ee4a85803230beff6364475516d3dfc03eb2
Nicolas Padilla <padillanico@gmail.com>
2023-01-05 13:10:33 -0300

    rutas arregladas

--- index.js
+++ index.js
@@ -21,15 +21,17 @@ app.use(express.json());
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/events',require('./routes/events'));
 
-app.get('*',(req,res)=>{
-    res.sendFile(__dirname + '/public/index.html');
-})
+
 
 
 
 //Directorio publico.
 
 app.use(express.static('public'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 //Escuchar peticiones.
 app.listen(process.env.PORT,()=>{
